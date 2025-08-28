947feced12f5cbb4d7ad3fed93faf90a4ceb1162
Luis Gutierrez <A00837481@tec.mx>
2025-08-28 12:27:29 -0600

    Prueba de movimiento de bala

--- MedioTermino/Assets/Scripts/Bullet.cs
+++ MedioTermino/Assets/Scripts/Bullet.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Velocidad de la bala
+    public float speed = 10f;
+
+    // Dirección de la bala
+    public Vector2 direction = Vector2.down;
+
+    // Vida de la bala en caso de no impactar con nada
+    private float maxLifetime = 5f;
+    private float lifetime = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Mover la bala en la dirección especificada
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        // Actualizar el tiempo de vida de la bala
+        lifetime += Time.deltaTime;
+        // Destruir la bala si ha excedido su tiempo de vida
+        if (lifetime > maxLifetime)
+        {
+            Disable();
+        }
     }
+
+    private void Disable()
+    {
+        Destroy(gameObject);
+    } 
 }
